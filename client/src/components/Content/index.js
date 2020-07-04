2d5e632bd8d7c1311ffd917d3787f33475f1f6de
Mahisha Gunasekaran <m.mahishamani@gmail.com>
2020-07-03 19:46:40 -0700

    Added animations to fontawesome icon

--- client/src/components/Content/index.js
+++ client/src/components/Content/index.js
@@ -1,20 +1,32 @@
 import React from "react";
+import { faAngleDoubleDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { bounce } from "react-animations";
+import styled, { keyframes } from "styled-components";
+const Bounce = styled.div`animation: 2s ${keyframes`${bounce}`} infinite`;
 
 function Content() {
-    const divStyle= {
+    const divStyle = {
         textAlign: "center",
         fontFamily: 'Pacifico',
         fontSize: "45px",
         color: "white"
     }
- return(
-     <div style={divStyle}>
-         Do you want something instantly for yourself and not owning it though? Well,
-         you are in the right place
+
+    return (
+        <div style={divStyle}>
+            Do you want something instantly for yourself and not owning it though? Well,
+            you are in the right place
             <br></br><br></br><br></br>
          Create an account with us and borrow anything you like from others!
-     </div>
- );
+
+
+            <br></br><br></br><br></br><br></br>
+            <Bounce>
+                <FontAwesomeIcon icon={faAngleDoubleDown} />
+            </Bounce>
+        </div>
+    );
 }
 
 export default Content;
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -16,36 +16,31 @@ function Navbar() {
 
     return (
         <nav className="navbar navbar-expand-lg navbar">
-            {/* <a className="navbar-brand" href="#">Navbar</a>
+            <a className="navbar-brand" href="#" style={headingStyle}>CONFER THINGS</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
-            </button> */}
+            </button>
 
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav m-auto text-light" style={headingStyle}>
-                    {/* <li className="nav-item active">
-                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                    <li className="nav-item active">
+                    <button 
+                    className="btn btn-outline-success my-2 my-sm-0" type="submit">SIGNUP
+                    </button>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
-                    </li>
-                    <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a className="dropdown-item" href="#">Action</a>
-                            <a className="dropdown-item" href="#">Another action</a>
-                            <div className="dropdown-divider"></div>
-                            <a className="dropdown-item" href="#">Something else here</a>
-                        </div>
+                    <button 
+                    className="btn btn-outline-success my-2 my-sm-0" type="submit">LOGIN
+                    </button>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link disabled" href="#">Disabled</a>
-                    </li> */}
-                    CONFER THINGS
+                        Your Things
+                    </li>
+                    <li className="nav-item">
+                        Rent It
+                    </li>
                 </ul>
-                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">LOGIN/SIGNUP</button>
+                
             </div>
         </nav>
     )
