81962ecc5820613fe0658c5aecdfd6e8e81af693
Mahisha Gunasekaran <m.mahishamani@gmail.com>
2020-07-23 13:22:15 -0700

    Included browser router to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './App.css';
 import About from "./components/About";
 
 function App() {
   return (
-    <About/>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={About} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
